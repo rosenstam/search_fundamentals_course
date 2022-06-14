7536a7353231c78774a185d2dced34212762fc58
rosenstam <rosenstam.joel@gmail.com>
2022-06-14 07:19:32 +0000

    merge week2

--- week1/index_products.py
+++ week1/index_products.py
@@ -1,4 +1,5 @@
 # From https://github.com/dshvadskiy/search_with_machine_learning_course/blob/main/index_products.py
+import opensearchpy
 import requests
 from lxml import etree
 
@@ -7,58 +8,82 @@ import glob
 from opensearchpy import OpenSearch
 from opensearchpy.helpers import bulk
 import logging
-import time
+
+from time import perf_counter
+import concurrent.futures
+
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(format='%(levelname)s:%(message)s')
 
 # NOTE: this is not a complete list of fields.  If you wish to add more, put in the appropriate XPath expression.
-# TODO: is there a way to do this using XPath/XSL Functions so that we don't have to maintain a big list?
-mappings = [
-    "sku/text()", "sku", # SKU is the unique ID, productIds can have multiple skus
-    "productId/text()", "productId",
-    "name/text()", "name",
-    "type/text()", "type",
-    "regularPrice/text()", "regularPrice",
-    "salePrice/text()", "salePrice",
-    "onSale/text()", "onSale",
-    "salesRankShortTerm/text()", "salesRankShortTerm",
-    "salesRankMediumTerm/text()", "salesRankMediumTerm",
-    "salesRankLongTerm/text()", "salesRankLongTerm",
-    "bestSellingRank/text()", "bestSellingRank",
-    "url/text()", "url",
-    "categoryPath/*/name/text()", "categoryPath",  # Note the match all here to get the subfields
-    "categoryPath/*/id/text()", "categoryPathIds",  # Note the match all here to get the subfields
-    "categoryPath/category[last()]/id/text()", "categoryLeaf",
-    "count(categoryPath/*/name)", "categoryPathCount",
-    "customerReviewCount/text()", "customerReviewCount",
-    "customerReviewAverage/text()", "customerReviewAverage",
-    "inStoreAvailability/text()", "inStoreAvailability",
-    "onlineAvailability/text()", "onlineAvailability",
-    "releaseDate/text()", "releaseDate",
-    "shortDescription/text()", "shortDescription",
-    "class/text()", "class",
-    "classId/text()", "classId",
-    "department/text()", "department",
-    "departmentId/text()", "departmentId",
-    "bestBuyItemId/text()", "bestBuyItemId",
-    "description/text()", "description",
-    "manufacturer/text()", "manufacturer",
-    "modelNumber/text()", "modelNumber",
-    "image/text()", "image",
-    "longDescription/text()", "longDescription",
-    "longDescriptionHtml/text()", "longDescriptionHtml",
-    "features/*/text()", "features"  # Note the match all here to get the subfields
+#TODO: is there a way to do this using XPath/XSL Functions so that we don't have to maintain a big list?
+mappings =  [
+            "productId/text()", "productId",
+            "sku/text()", "sku",
+            "name/text()", "name",
+            "type/text()", "type",
+            "startDate/text()", "startDate",
+            "active/text()", "active",
+            "regularPrice/text()", "regularPrice",
+            "salePrice/text()", "salePrice",
+            "artistName/text()", "artistName",
+            "onSale/text()", "onSale",
+            "digital/text()", "digital",
+            "frequentlyPurchasedWith/*/text()", "frequentlyPurchasedWith",# Note the match all here to get the subfields
+            "accessories/*/text()", "accessories",# Note the match all here to get the subfields
+            "relatedProducts/*/text()", "relatedProducts",# Note the match all here to get the subfields
+            "crossSell/text()", "crossSell",
+            "salesRankShortTerm/text()", "salesRankShortTerm",
+            "salesRankMediumTerm/text()", "salesRankMediumTerm",
+            "salesRankLongTerm/text()", "salesRankLongTerm",
+            "bestSellingRank/text()", "bestSellingRank",
+            "url/text()", "url",
+            "categoryPath/*/name/text()", "categoryPath", # Note the match all here to get the subfields
+            "categoryPath/*/id/text()", "categoryPathIds", # Note the match all here to get the subfields
+            "categoryPath/category[last()]/id/text()", "categoryLeaf",
+            "count(categoryPath/*/name)", "categoryPathCount",
+            "customerReviewCount/text()", "customerReviewCount",
+            "customerReviewAverage/text()", "customerReviewAverage",
+            "inStoreAvailability/text()", "inStoreAvailability",
+            "onlineAvailability/text()", "onlineAvailability",
+            "releaseDate/text()", "releaseDate",
+            "shippingCost/text()", "shippingCost",
+            "shortDescription/text()", "shortDescription",
+            "shortDescriptionHtml/text()", "shortDescriptionHtml",
+            "class/text()", "class",
+            "classId/text()", "classId",
+            "subclass/text()", "subclass",
+            "subclassId/text()", "subclassId",
+            "department/text()", "department",
+            "departmentId/text()", "departmentId",
+            "bestBuyItemId/text()", "bestBuyItemId",
+            "description/text()", "description",
+            "manufacturer/text()", "manufacturer",
+            "modelNumber/text()", "modelNumber",
+            "image/text()", "image",
+            "condition/text()", "condition",
+            "inStorePickup/text()", "inStorePickup",
+            "homeDelivery/text()", "homeDelivery",
+            "quantityLimit/text()", "quantityLimit",
+            "color/text()", "color",
+            "depth/text()", "depth",
+            "height/text()", "height",
+            "weight/text()", "weight",
+            "shippingWeight/text()", "shippingWeight",
+            "width/text()", "width",
+            "longDescription/text()", "longDescription",
+            "longDescriptionHtml/text()", "longDescriptionHtml",
+            "features/*/text()", "features" # Note the match all here to get the subfields
 
-]
-
+        ]
 
 def get_opensearch():
     host = 'localhost'
     port = 9200
     auth = ('admin', 'admin')
-
     #### Step 2.a: Create a connection to OpenSearch
     client = OpenSearch(
         hosts=[{'host': host, 'port': port}],
@@ -74,6 +99,29 @@ def get_opensearch():
     )
     return client
 
+
+def index_file(file, index_name):
+    docs_indexed = 0
+    client = get_opensearch()
+    logger.info(f'Processing file : {file}')
+    tree = etree.parse(file)
+    root = tree.getroot()
+    children = root.findall("./product")
+    docs = []
+    for child in children:
+        doc = {}
+        for idx in range(0, len(mappings), 2):
+            xpath_expr = mappings[idx]
+            key = mappings[idx + 1]
+            doc[key] = child.xpath(xpath_expr)
+        #print(doc)
+        if 'productId' not in doc or len(doc['productId']) == 0:
+            continue
+        #### Step 2.b: Create a valid OpenSearch Doc and bulk index 2000 docs at a time
+        the_doc = None
+        docs.append(the_doc)
+
+    return docs_indexed
 
 @click.command()
 @click.option('--source_dir', '-s', help='XML files source directory')
@@ -114,6 +162,8 @@ def main(source_dir: str, index_name: str):
     toc = time.perf_counter()
     logger.info(f'Done. Total docs: {docs_indexed}.  Total time: {((toc - tic) / 60):0.3f} mins.')
 
+    finish = perf_counter()
+    logger.info(f'Done. Total docs: {docs_indexed} in {(finish - start)/60} minutes')
 
 if __name__ == "__main__":
     main()
